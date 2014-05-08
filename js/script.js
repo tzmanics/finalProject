18c5e6a3406d564e4a9c17f439e598a35e6ee232
tzmanics <tzmanics@gmail.com>
2014-05-08 09:33:46 -0400

    small content & style changes, +js initial file

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function init() {
+	// SLIDE SHOW PLANS
+	// change styling so sections fit to screen height
+	// ?? hide overflow so only one section at a time shows
+	// add left and right slideshow nav buttons
+	// add event listener for buttons to move viewable content
+}
+
+window.onload = init;
