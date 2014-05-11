1f2121bf2414124c5e17c4c8d54b75e17914f132
tzmanics <tzmanics@gmail.com>
2014-05-11 12:38:25 -0400

    + JS to position section contents

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
 function init() {
 	// SLIDE SHOW PLANS
-	// change styling so sections fit to screen height
-	// ?? hide overflow so only one section at a time shows
-	// add left and right slideshow nav buttons
-	// add event listener for buttons to move viewable content
+	// var sections = document.getElementsByTagName('section');
+	// for(var i = 0; i < sections.length; i++){
+	// 	var section = sections[i];
+	// 	section.style.height = window.innerHeight;
+	// 	// console.log(window.innerHeight);
+	// };
+
+	// apply position to #webDev.project
+	var webProjects = document.getElementById('webDev').getElementsByClassName('project');
+	for(var i = 0; i < webProjects.length; i++){
+		webProjects[i].style.position = "relative";
+		webProjects[i].style.display = "none";
+	}
+	// display the first element
+	webProjects[0].style.display = "block";
+	// apply position to #mobile.project
+	var mobileProjects = document.getElementById('mobile').getElementsByClassName('project');
+	for(var i = 0; i < mobileProjects.length; i++){
+		mobileProjects[i].style.position = "relative";
+		mobileProjects[i].style.display = "none";
+	}
+	// display the first element
+	mobileProjects[0].style.display = "block";
+
 }
 
 window.onload = init;
