d55da6eb31c8f409e9d77bd0243e0bf5f5095b1f
tzmanics <tzmanics@gmail.com>
2014-05-11 19:24:05 -0400

    + slideshow nav (not DRY)

--- js/script.js
+++ js/script.js
@@ -8,21 +8,116 @@ function init() {
 	// };
 
 	// apply position to #webDev.project
-	var webProjects = document.getElementById('webDev').getElementsByClassName('project');
-	for(var i = 0; i < webProjects.length; i++){
-		webProjects[i].style.position = "relative";
-		webProjects[i].style.display = "none";
-	}
-	// display the first element
-	webProjects[0].style.display = "block";
+	(function webSlides(){
+		var webProjects = document.getElementById('webDev').getElementsByClassName('project');
+		for(var i = 0; i < webProjects.length; i++){
+			webProjects[i].style.position = "relative";
+			webProjects[i].style.display = "none";
+		};
+		// display the first element
+		webProjects[0].style.display = "block";
+		// prev slide
+		var i = 0;
+		var prevButton = document.createElement("BUTTON");
+		prevButton.className = "prevButton";
+		document.getElementById('webDev').appendChild(prevButton);
+		prevButton.addEventListener('click', function(){
+			(function prevSlide(){
+				if( i > 0){
+					webProjects[i].style.display = "none";
+					webProjects[i-1].style.display = "block";
+					return i = i -1;
+				}else{
+					webProjects[0].style.display = "none";
+					webProjects[webProjects.length-1].style.display = "block";
+					return i = webProjects.length-1;
+				};
+			})();
+		});
+		//next slide
+		var nextButton = document.createElement("BUTTON");
+		nextButton.className = "nextButton";
+		document.getElementById('webDev').appendChild(nextButton);
+		nextButton.addEventListener('click', function(){
+			(function nextSlide(){
+				if( i < webProjects.length-1){
+					webProjects[i].style.display = "none";
+					webProjects[i+1].style.display = "block";
+					return i = i +1;
+				}else{
+					webProjects[i].style.display = "none";
+					webProjects[0].style.display = "block";
+					return i = 0;
+				};
+			})();
+		});
+	})();
 	// apply position to #mobile.project
-	var mobileProjects = document.getElementById('mobile').getElementsByClassName('project');
-	for(var i = 0; i < mobileProjects.length; i++){
-		mobileProjects[i].style.position = "relative";
-		mobileProjects[i].style.display = "none";
+	(function mobileSlides(){
+		var mobileProjects = document.getElementById('mobile').getElementsByClassName('project');
+		for(var i = 0; i < mobileProjects.length; i++){
+			mobileProjects[i].style.position = "relative";
+			mobileProjects[i].style.display = "none";
+		}
+		// display the first element
+		mobileProjects[0].style.display = "block";
+		//prev slide
+		var i = 0;
+		var mobPrevButton = document.createElement("BUTTON");
+		mobPrevButton.className = "prevButton";
+		document.getElementById('mobile').appendChild(mobPrevButton);
+		mobPrevButton.addEventListener('click', function(){
+			(function prevSlide(){
+				if( i > 0){
+					mobileProjects[i].style.display = "none";
+					mobileProjects[i-1].style.display = "block";
+					return i = i -1;
+				}else{
+					mobileProjects[0].style.display = "none";
+					mobileProjects[mobileProjects.length-1].style.display = "block";
+					return i = mobileProjects.length-1;
+				};
+			})();
+		});
+		//next slide
+		var mobNextButton = document.createElement("BUTTON");
+		mobNextButton.className = "nextButton";
+		document.getElementById('mobile').appendChild(mobNextButton);
+		mobNextButton.addEventListener('click', function(){
+			(function nextSlide(){
+				if( i < mobileProjects.length-1){
+					mobileProjects[i].style.display = "none";
+					mobileProjects[i+1].style.display = "block";
+					return i = i +1;
+				}else{
+					mobileProjects[i].style.display = "none";
+					mobileProjects[0].style.display = "block";
+					return i = 0;
+				};
+			})();
+		});
+	})();
+
+	// navigation
+	var navElems = document.getElementsByTagName('li');
+	for(var i = 0; i < navElems.length; i++){
+		navElems[i].style.display = "none";
 	}
-	// display the first element
-	mobileProjects[0].style.display = "block";
+	var burger = document.getElementById('hamburger');
+	burger.addEventListener('click', function(){
+		if(navElems[0].style.display == "none"){
+			for(var i = 0; i < navElems.length; i++){
+				navElems[i].style.display = "block";
+			}
+		} else {
+			for(var i = 0; i < navElems.length; i++){
+				navElems[i].style.display = "none";
+			}
+		}
+	});
+
+	
+
 
 }
 
